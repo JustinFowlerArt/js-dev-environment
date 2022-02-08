6531917cc5e2d0d73a71db28e3687ed3359294ae
Justin Fowler <jusfowle@gmail.com>
2022-02-08 14:23:34 -0500

    Add npm, Express, Babel, Webpack, and Mocha

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+/* import numeral from "numeral";
+
+const CourseValue = numeral(1000).format("$0,0.00");
+debugger;
+console.log(`I would pay ${CourseValue} for this awesome course!`) */
